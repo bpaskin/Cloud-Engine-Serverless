d10eb09ed64e0c2c53e8484c990432887925d8f6
Brian S Paskin <brian@paskino.com>
2022-07-07 06:49:42 -0400

    update to vote

--- acceptVote/src/main/java/com/ibm/example/serverless/AcceptVote.java
+++ acceptVote/src/main/java/com/ibm/example/serverless/AcceptVote.java
@@ -10,33 +10,60 @@ import java.net.InetSocketAddress;
 
 public class AcceptVote implements HttpHandler {
 
-	public static void main(String[] args) throws Exception {
-		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-		server.createContext("/", new AcceptVote());
-		server.start();
+	public static void main(String[] args) {
+		
+		try {
+			// Setup HTTP Server to accept requests on port 8080
+			// All requests coming in on / will be routed to AcceptVote()
+			HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+			server.createContext("/", new AcceptVote());
+			server.start();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
 
-		System.out.println("Welcome to my world!");
+		System.out.println("Accept Vote is running!");
 	}
 
+	@Override
 	public void handle(HttpExchange t) throws IOException {
+		
+		// setup response
+		int responseCode = 200;
+		String response = "ok";
+		
+		// get the form data sent from the HTTP Page
         StringBuilder sb = new StringBuilder();
         InputStream ios = t.getRequestBody();
+        
         int i;
         while ((i = ios.read()) != -1) {
             sb.append((char) i);
         }
-        System.out.println("hm: " + sb.toString());
+        
+        ios.close();
 		
-		String response;
+        // make sure there is a selection
+        String[] parts;
+        
+        if (sb.length() == 0) {
+        	responseCode = 400;
+        }
+        
+        if (sb.length() > 0) {
+        	parts = sb.toString().split("&");
+        	
+        	if (parts.length == 1) {
+        		
+        	} else {
+        		
+        	}
+        }
+       
+        
+
 		
-		if ( t.getAttribute("pizza") == null) {
-			 response = "no pizza found";
-		} else {
-			 response = "This is the response";
-	
-		}
-		
-		t.sendResponseHeaders(200, response.length());
+		t.sendResponseHeaders(responseCode, response.length());
 		OutputStream os = t.getResponseBody();
 		os.write(response.getBytes());
 		os.close();
